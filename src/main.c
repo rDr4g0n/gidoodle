9ea5af1bff8396f9a20807f82df024d89972255d
John Matos <rDr4g0n@gmail.com>
2016-02-10 09:10:02 -0600

    command line opts!

--- src/main.c
+++ src/main.c
@@ -10,11 +10,12 @@
 #include <time.h>
 #include <string.h>
 #include <stdbool.h>
+#include <argp.h>
 
 #include "arglist.h"
 #include "mousebox.h"
 
-#define DEBUG 0
+bool DEBUG = false;
 
 void debug(const char *fmt, ...) {
     if(DEBUG){
@@ -34,6 +35,9 @@ int timestamp(){
 #define MAX_FILTER_LENGTH 100
 
 typedef struct Config {
+    // for argp
+    char * args[2];
+
     char tempDir[MAX_PATH_LENGTH];
     char outputPath[MAX_PATH_LENGTH];
     int fps;
@@ -50,45 +54,120 @@ typedef struct Config {
     // TODO - skip palette optimization?
 } Config;
 
-Config * buildConfig(){
+
+
+
+const char *argp_program_version = "0.1.1alpha";
+const char *argp_program_bug_address = "jmatos@zenoss.com";
+int parse_opt(int key, char * arg, struct argp_state * state){
+    if(DEBUG && arg != NULL){
+        printf("key: %c(%i), arg: %s\n", key, key, arg);
+    }
+    struct Config * config = state->input;
+
+    switch(key){
+        // output file
+        case 'o':
+            strcpy(config->outputPath, arg);
+            // TODO - get full path if path is relative
+            break;
+
+        // scale
+        case 's':
+            sscanf(arg, "%f", &config->scale);
+            break;
+        // start delay
+        case 'd':
+            sscanf(arg, "%i", &config->startDelay);
+            break;
+        // debug messages
+        case 'v':
+            DEBUG = true;
+            break;
+        // preserve-temp
+        case 128:
+            config->preserveTemp = true;
+            break;
+        // fps
+        case 129:
+            sscanf(arg, "%i", &config->fps);
+            break;
+
+        case ARGP_KEY_ARG:
+            // shorthand for output file
+            // TODO - this probably aint right
+            strcpy(config->outputPath, arg);
+            break;
+        case ARGP_KEY_END:
+            break;
+        default:
+            return ARGP_ERR_UNKNOWN;
+    }
+    return 0;
+}
+
+Config * buildConfig(int argc, char **argv){
     Config * config = malloc(sizeof(Config));
 
-    // TODO - take command line stuff
-    // temp dir, output file, fps, scale, start delay, preserve temp
+    // default configs
+    strcpy(config->outputPath, "/home/john/gidoos/mygif.gif");
+    config->fps = 25;
+    config->scale = 0.75;
+    config->startDelay = 0;
+    config->preserveTemp = true;
     // TODO - let user specify capture rectangle via cli
-    strcpy(config->tempDir, "/home/jay/tmp");
-    strcpy(config->outputPath, "/home/jay/tmp/mygif.gif");
+    // TODO - upload gif to places
+
+    // apply cli opts
+    // NOTE - overrides defaults
+    static struct argp_option options[] = {
+        // TODO - grab output as the last arg with no flag needed
+        {"output", 'o', "OUTFILE", 0, "Output file path and name", 0},
+        {"scale", 's', "SCALE", 0, "Apply scaling to gif, 0 - 1 (default 1)", 0},
+        {"delay", 'd', "DELAY", 0, "Delay in seconds before recording begins (default 0)", 0},
+        {"verbose", 'v', 0, 0, "Enable debug messages", 0},
+        {"preserve-temp", 128, 0, 0, "Preserve temp files (default false)", 0},
+        {"fps", 129, "FPS", 0, "Capture and output framerate (default 30)", 0},
+        {0}
+    };
+    static struct argp argp = {options, parse_opt, "outfile.gif", "Draw a rectangle, do stuff, get a gif!", 0, 0, 0};
+    argp_parse(&argp, argc, argv, 0, 0, config);
+
+    // calculated configy stuff
+    // NOTE - overrides defaults and CLI configs
+    strcpy(config->tempDir, "/tmp");
     // TODO - get ffmpeg path from os
     strcpy(config->ffmpegpath, "/usr/bin/ffmpeg");
-    config->fps = 25;
-
     snprintf(config->id, MAX_ID_LENGTH, "%i", timestamp());
-
+    // tmp video
     strcpy(config->vidpath, config->tempDir);
     strcat(config->vidpath, "/");
     strcat(config->vidpath, config->id);
     strcat(config->vidpath, ".mkv");
-
+    // tmp log
+    // TODO - append to a running log of all jobs?
     strcpy(config->logPath, config->tempDir);
     strcat(config->logPath, "/");
     strcat(config->logPath, config->id);
     strcat(config->logPath, ".log");
-
+    // tmp palette
     strcpy(config->palette, config->tempDir);
     strcat(config->palette, "/");
     strcat(config->palette, config->id);
     strcat(config->palette, ".png");
-
-    config->scale = 1;
-    config->startDelay = 0;
-    config->preserveTemp = true;
-
+    // filter string
     snprintf(config->filters, MAX_FILTER_LENGTH,
         "fps=%i,scale=iw*%f:ih*%f:flags=lanczos",
         config->fps, config->scale, config->scale);
 
     return config;
 }
+
+
+
+
+
+
 
 int openLogFile(char * filePath){
     int fd = open(filePath, O_WRONLY|O_CREAT, 0664);
@@ -114,6 +193,7 @@ pid_t doAThing(ArgList * command, int fd){
         dup2(fd, STDOUT_FILENO);
         close(fd);
         execv(command->list[0], command->list);
+        printf("ERROR: child asplode");
         _exit(1);
     }
 
@@ -140,6 +220,7 @@ int captureVideo(char * ffmpegpath, rect * r, int fps, char * vidpath, int start
     pushArg(cmd, "-preset");
     pushArg(cmd, "ultrafast");
     pushArg(cmd, "-y");
+    pushArg(cmd, "-hide_banner");
     pushFArg(cmd, "%s", vidpath);
     endArgList(cmd);
     if(DEBUG){
@@ -177,6 +258,7 @@ int generatePalette(char * ffmpegpath, char * vidpath, char * filters, char * pa
     pushArg(cmd, "-vf");
     pushFArg(cmd, "%s,palettegen", filters);
     pushArg(cmd, "-y");
+    pushArg(cmd, "-hide_banner");
     pushFArg(cmd, "%s", palette);
     endArgList(cmd);
     if(DEBUG){
@@ -203,6 +285,7 @@ int gifify(char * ffmpegpath, char * vidpath, char * palette, char * filters, in
     pushArg(cmd, "-lavfi");
     pushFArg(cmd, "%s [x]; [x][1:v] paletteuse", filters);
     pushArg(cmd, "-y");
+    pushArg(cmd, "-hide_banner");
     pushFArg(cmd, "%s.gif", vidpath);
     endArgList(cmd);
     if(DEBUG){
@@ -219,8 +302,11 @@ int gifify(char * ffmpegpath, char * vidpath, char * palette, char * filters, in
     return status;
 }
 
-int main(){
-    Config * config = buildConfig();
+int main(int argc, char **argv){
+    Config * config = buildConfig(argc, argv);
+
+    debug("Config\n outputPath: '%s'\n, fps: %i\n, scale: %f\n, startDelay: %i\n, verbose: %i\n",
+            config->outputPath, config->fps, config->scale, config->startDelay, DEBUG);
 
     printf("Draw a rectangle over the area you want to capture\n");
     rect * r = getBoundingBox();
